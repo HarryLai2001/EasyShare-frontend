31b71efcbb5024004547dbf36121050ffe78a868
HarryLai2001 <143411943+HarryLai2001@users.noreply.github.com>
2024-11-13 12:45:10 +0800

    Update url.jsx

--- src/renderer/constants/url.jsx
+++ src/renderer/constants/url.jsx
@@ -7,4 +7,4 @@ export const SOCKET_DEV_URL = '*****************'
 export const SOCKET_PROD_URL = '*********************'
 
 export const DEFAULT_AVATAR =
-  'https://lizehan.oss-cn-beijing.aliyuncs.com/avatar/default-avatar.png'
+  '********************************'
